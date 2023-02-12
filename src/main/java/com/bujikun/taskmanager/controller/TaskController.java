51b15e10e44a07bc17b966f78666faac288b596e
Newton Bujiku <bujikun@vodacom.co.tz>
2023-02-12 13:16:28 +0300

    Add task controller methods  to produce task addition form and the logic that handles task creation from the controller

--- src/main/java/com/bujikun/taskmanager/controller/TaskController.java
+++ src/main/java/com/bujikun/taskmanager/controller/TaskController.java
@@ -1,10 +1,13 @@
 package com.bujikun.taskmanager.controller;
 
+import com.bujikun.taskmanager.dto.TaskDTO;
 import com.bujikun.taskmanager.service.contract.service.implementation.TaskService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
@@ -17,4 +20,19 @@ public class TaskController {
         page.addAttribute("tasks",taskService.findAll());
         return "task/index";
     }
+
+    @GetMapping("/tasks/add")
+    public String getTaskAdditionForm(){
+        return "/task/add";
+    }
+    @PostMapping
+    public String createNewTask(@RequestBody TaskDTO taskDTO, Model page){
+        //do validation
+        taskService.createTask(taskDTO);
+        page.addAttribute("tasks",taskService.findAll());
+        return "task/index";
+    }
+
+
+
 }
