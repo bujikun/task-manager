6de8e6beca53527b4ed0f82f6d8b69e96006162c
Newton Bujiku <bujikun@vodacom.co.tz>
2023-02-18 23:30:44 +0300

    Minor UI changes Add in memory managed user for testing Enable debug

--- src/main/java/com/bujikun/taskmanager/security/config/AppSecurityConfig.java
+++ src/main/java/com/bujikun/taskmanager/security/config/AppSecurityConfig.java
@@ -3,6 +3,11 @@ package com.bujikun.taskmanager.security.config;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.crypto.argon2.Argon2PasswordEncoder;
+import org.springframework.security.crypto.factory.PasswordEncoderFactories;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
 
 /**
@@ -25,8 +30,31 @@ public class AppSecurityConfig {
                .formLogin(f->f.loginPage("/login")
                                .failureForwardUrl("/login")
                                .defaultSuccessUrl("/")
+                               .usernameParameter("username")
+                               .passwordParameter("password")
                                .isCustomLoginPage()
                        )
+               .logout(l->l.logoutUrl("/logout")
+                       .logoutSuccessUrl("/login")
+                       .invalidateHttpSession(true)
+                       .clearAuthentication(true)
+                       .permitAll()
+                       )
                .build();
+    }
+
+    @Bean
+    public PasswordEncoder passwordEncoder(){
+        return PasswordEncoderFactories.createDelegatingPasswordEncoder();
+    }
+
+    @Bean
+    public InMemoryUserDetailsManager userDetailsService(){
+        var user = User.withUsername("admin")
+                .password("password")
+                .passwordEncoder(p-> passwordEncoder().encode(p))
+                .authorities("read")
+                .build();
+        return new InMemoryUserDetailsManager(user);
     }
 }
